9c361ef0d0e11b75a7b8f3fc2b8e92d0ee1d080c
Gabriel Ferrari Ceron <gferrari.ceron@outlook.com>
2022-06-17 16:36:45 -0300

    Comentários e sugestões

--- src/main/kotlin/system/cli/business/ManagerProduct.kt
+++ src/main/kotlin/system/cli/business/ManagerProduct.kt
@@ -7,10 +7,21 @@ class ManagerProduct() {
 
     companion object{
         fun registerProduct(product: Product):Boolean{
+
+            // Talvez tenha um erro de conceito aqui
+            // Toda vez que você adiciona um produto, a classe ProductList é instanciada.
+            // O que significa que seu 'banco de dados' é inicializado e os produtos são perdidos.
             val database = ProductList(product)
             database.productListDb.add(product)
-            println("Registrando Produto: $product.name")
-return true
+
+            // Essa interpolação do Kotlin faltou só uma chaves pra ficar 100%
+            // println("Registrando Produto: ${product.name}")
+
+            // Mas com a mudança de data class, talvez possa deixar print no objeto
+            // Aí depende do gosto
+            println("Registrando Produto: $product")
+
+            return true
         }
     }
 
--- src/main/kotlin/system/cli/entity/Product.kt
+++ src/main/kotlin/system/cli/entity/Product.kt
@@ -1,2 +1,6 @@
 package system.cli.entity
-class Product(val nome:String,val preco:Float)
+
+// Já chegou em data class?
+// Acho que nesse caso que só transita dados funciona bem
+// e funciona muito bem quando faz um println na classe, já dá pra ver os valores legíveis
+data class Product(val nome:String,val preco:Float)
